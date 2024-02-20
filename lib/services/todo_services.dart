959f454a751d9fccab3470ede7f49cd67a0d6154
LITTAVANH PHANALASY <littavanhphanalasy@gmail.com>
2024-02-20 15:27:30 +0700

    dd

--- lib/services/todo_services.dart
+++ lib/services/todo_services.dart
@@ -5,6 +5,7 @@ final fm = NumberFormat("#,###.##", 'en_US');
 final fmdate = DateFormat("dd/MM/yyyy", 'en_US');
 // String url = 'http://192.168.100.28:8443/api/';
 
-String url = 'https://hrm.tkgroup.la:8822/api/';
+String url = 'http://hrm.tkgroup.la:8822/api/';
+// String url = 'https://hrm.tkgroup.la:7443/api/';
 
 const String publickey = 'wt86t129-58m0-c245-8ff0-80ynov0a29pmh';
